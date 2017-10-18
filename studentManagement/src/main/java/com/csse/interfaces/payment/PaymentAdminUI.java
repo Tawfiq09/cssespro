d39d8fd0c027af0daca96f3955cbf1da7568b151
sajithJanadara <sajithjayasekara123@gmail.com>
2017-10-18 19:05:38 +0530

    add payment admin UI

---
+++ studentManagement/src/main/java/com/csse/interfaces/payment/PaymentAdminUI.java
@@ -0,0 +1,46 @@
+package com.csse.interfaces.payment;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PaymentAdminUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PaymentAdminUI frame = new PaymentAdminUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PaymentAdminUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
--- studentManagement/src/main/java/com/csse/payment/semester_payment/SemesterPayment.java
+++ studentManagement/src/main/java/com/csse/payment/semester_payment/SemesterPayment.java
@@ -1,5 +1,109 @@
 package com.csse.payment.semester_payment;
 
+import java.util.Date;
+
 public class SemesterPayment {
+
+	// student details
+	private String studentId;
+	private String studentName;
+	private String studentEmail;
+	private Integer year;
+	private String semester;
+	private String Faculty;
+	private String specialication;
+	private double courseFee;
+	// bank details
+	private String bankName;
+	private String branchName;
+	private Date date;
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getSemester() {
+		return semester;
+	}
+
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+
+	public String getFaculty() {
+		return Faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		Faculty = faculty;
+	}
+
+	public String getSpecialication() {
+		return specialication;
+	}
+
+	public void setSpecialication(String specialication) {
+		this.specialication = specialication;
+	}
+
+	public double getCourseFee() {
+		return courseFee;
+	}
+
+	public void setCourseFee(double courseFee) {
+		this.courseFee = courseFee;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getBranchName() {
+		return branchName;
+	}
+
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 }
