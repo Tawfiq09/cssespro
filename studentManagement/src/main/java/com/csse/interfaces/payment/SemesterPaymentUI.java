270150c5090330a6677b7bdfe3f03bc2bc1747d2
sajithJanadara <sajithjayasekara123@gmail.com>
2017-10-19 17:16:58 +0530

    did some changes to sevelral payment classes

--- studentManagement/src/main/java/com/csse/interfaces/payment/SemesterPaymentUI.java
+++ studentManagement/src/main/java/com/csse/interfaces/payment/SemesterPaymentUI.java
@@ -11,7 +11,11 @@ import com.toedter.calendar.JDateChooser;
 import javax.swing.JComboBox;
 import java.awt.Color;
 import java.awt.Font;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
 
 public class SemesterPaymentUI extends JFrame {
 
@@ -50,128 +54,142 @@ public class SemesterPaymentUI extends JFrame {
 	public SemesterPaymentUI() {
 		setTitle("Semester Registration");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 723, 487);
+		setBounds(100, 100, 750, 515);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblStudentId = new JLabel("Student ID");
-		lblStudentId.setBounds(24, 61, 86, 14);
+		lblStudentId.setBounds(24, 60, 86, 14);
 		contentPane.add(lblStudentId);
-		
+
 		JLabel lblStudentName = new JLabel("Student Name");
-		lblStudentName.setBounds(24, 101, 86, 14);
+		lblStudentName.setBounds(24, 100, 86, 14);
 		contentPane.add(lblStudentName);
-		
+
 		JLabel lblStudentEmail = new JLabel("Student Email");
-		lblStudentEmail.setBounds(24, 141, 86, 14);
+		lblStudentEmail.setBounds(24, 140, 86, 14);
 		contentPane.add(lblStudentEmail);
-		
+
 		JLabel lblYear = new JLabel("Year");
-		lblYear.setBounds(24, 181, 86, 14);
+		lblYear.setBounds(24, 220, 86, 14);
 		contentPane.add(lblYear);
-		
+
 		JLabel lblSemester = new JLabel("Semester");
-		lblSemester.setBounds(24, 221, 86, 14);
+		lblSemester.setBounds(24, 260, 86, 14);
 		contentPane.add(lblSemester);
-		
+
 		JLabel lblFaculty = new JLabel("Faculty");
 		lblFaculty.setBounds(372, 54, 86, 14);
 		contentPane.add(lblFaculty);
-		
+
 		JLabel lblSpecialication = new JLabel("Specialication");
 		lblSpecialication.setBounds(372, 94, 86, 14);
 		contentPane.add(lblSpecialication);
-		
+
 		JLabel lblCourseFee = new JLabel("Course Fee");
 		lblCourseFee.setBounds(372, 134, 86, 14);
 		contentPane.add(lblCourseFee);
-		
-		JLabel lblRegisteredDate = new JLabel("Registered Date");
+
+		JLabel lblRegisteredDate = new JLabel("Registration Date");
 		lblRegisteredDate.setBounds(372, 174, 98, 14);
 		contentPane.add(lblRegisteredDate);
-		
+
 		textFieldStudentID = new JTextField();
 		textFieldStudentID.setBounds(158, 58, 100, 20);
 		contentPane.add(textFieldStudentID);
 		textFieldStudentID.setColumns(10);
-		
+
 		textFieldStudentName = new JTextField();
 		textFieldStudentName.setBounds(158, 98, 100, 20);
 		contentPane.add(textFieldStudentName);
 		textFieldStudentName.setColumns(10);
-		
+
 		textFieldStudentEmail = new JTextField();
 		textFieldStudentEmail.setBounds(158, 138, 100, 20);
 		contentPane.add(textFieldStudentEmail);
 		textFieldStudentEmail.setColumns(10);
-		
+
 		JDateChooser dateChooserRegisterdDate = new JDateChooser();
 		dateChooserRegisterdDate.setBounds(509, 168, 100, 20);
 		contentPane.add(dateChooserRegisterdDate);
-		
+		dateChooserRegisterdDate.setMinSelectableDate(new Date());
+
 		textFieldYear = new JTextField();
-		textFieldYear.setBounds(158, 178, 100, 20);
+		textFieldYear.setEditable(false);
+		textFieldYear.setBounds(158, 218, 100, 20);
 		contentPane.add(textFieldYear);
 		textFieldYear.setColumns(10);
-		
-		JComboBox comboBoxSemester = new JComboBox();
-		comboBoxSemester.setBounds(158, 218, 100, 20);
+		String year = Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
+		textFieldYear.setText(year);
+
+		JComboBox<String> comboBoxSemester = new JComboBox<String>();
+		comboBoxSemester.setModel(new DefaultComboBoxModel<String>(new String[] {"select", "1", "2"}));
+		comboBoxSemester.setBounds(158, 258, 100, 20);
 		contentPane.add(comboBoxSemester);
-		
-		JComboBox comboBoxFaculty = new JComboBox();
+
+		JComboBox<String> comboBoxFaculty = new JComboBox<String>();
 		comboBoxFaculty.setBounds(509, 51, 100, 20);
 		contentPane.add(comboBoxFaculty);
-		
-		JComboBox comboBoxSpecialication = new JComboBox();
+
+		JComboBox<String> comboBoxSpecialication = new JComboBox<String>();
 		comboBoxSpecialication.setBounds(509, 91, 100, 20);
 		contentPane.add(comboBoxSpecialication);
-		
+
 		textFieldCourseFee = new JTextField();
 		textFieldCourseFee.setBounds(509, 131, 100, 20);
 		contentPane.add(textFieldCourseFee);
 		textFieldCourseFee.setColumns(10);
-		
+
 		JLabel lblBankDetails = new JLabel("Bank Details");
 		lblBankDetails.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblBankDetails.setForeground(Color.BLACK);
-		lblBankDetails.setBounds(24, 279, 105, 14);
+		lblBankDetails.setBounds(24, 324, 105, 14);
 		contentPane.add(lblBankDetails);
-		
+
 		JLabel lblBank = new JLabel("Bank");
-		lblBank.setBounds(24, 335, 46, 14);
+		lblBank.setBounds(24, 380, 46, 14);
 		contentPane.add(lblBank);
-		
+
 		JLabel lblBranch = new JLabel("Branch");
-		lblBranch.setBounds(24, 378, 46, 14);
+		lblBranch.setBounds(24, 423, 46, 14);
 		contentPane.add(lblBranch);
-		
+
 		textFieldBank = new JTextField();
-		textFieldBank.setBounds(95, 332, 100, 20);
+		textFieldBank.setBounds(95, 377, 100, 20);
 		contentPane.add(textFieldBank);
 		textFieldBank.setColumns(10);
-		
+
 		textFieldbranch = new JTextField();
-		textFieldbranch.setBounds(95, 375, 100, 20);
+		textFieldbranch.setBounds(95, 420, 100, 20);
 		contentPane.add(textFieldbranch);
 		textFieldbranch.setColumns(10);
-		
+
 		JLabel lblDeposit = new JLabel("Deposit Date");
-		lblDeposit.setBounds(236, 335, 98, 14);
+		lblDeposit.setBounds(236, 380, 98, 14);
 		contentPane.add(lblDeposit);
-		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(333, 329, 100, 20);
-		contentPane.add(dateChooser);
-		
+
+		JDateChooser dateChooserDeposit = new JDateChooser();
+		dateChooserDeposit.setBounds(333, 374, 100, 20);
+		contentPane.add(dateChooserDeposit);
+
 		JButton btnSubmit = new JButton("SUBMIT");
-		btnSubmit.setBounds(531, 334, 98, 58);
+		btnSubmit.setBounds(578, 379, 98, 58);
 		contentPane.add(btnSubmit);
-		
+
 		JLabel lblStudentDetails = new JLabel("Student Details");
 		lblStudentDetails.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblStudentDetails.setBounds(24, 11, 128, 14);
 		contentPane.add(lblStudentDetails);
+		
+		JLabel lblStudentCurrentYear = new JLabel("Student Current Year");
+		lblStudentCurrentYear.setBounds(24, 180, 116, 14);
+		contentPane.add(lblStudentCurrentYear);
+		
+		JComboBox<String> comboBoxstudentCurrentYear = new JComboBox<String>();
+		comboBoxstudentCurrentYear.setModel(new DefaultComboBoxModel<String>(new String[] {"select", "1", "2", "3", "4"}));
+		comboBoxstudentCurrentYear.setBounds(158, 178, 100, 20);
+		contentPane.add(comboBoxstudentCurrentYear);
 	}
 }
--- studentManagement/src/main/java/com/csse/payment/DBConnection.java
+++ studentManagement/src/main/java/com/csse/payment/DBConnection.java
@@ -1,5 +1,27 @@
 package com.csse.payment;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+	public static Connection getconnection() throws ClassNotFoundException {
+
+		String driver = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/cssemyoriginal";
+		String userName = "root";
+		String password = "sagarox";
+		Class.forName(driver);
+		try {
+			Connection connection = DriverManager.getConnection(url, userName, password);
+
+			return connection;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+
+			System.out.println(e);
+		}
+		return null;
+	}
 }
