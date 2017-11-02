04aa76b4a58411634955d9cf59feeb409568deda
sajithJanadara <sajithjayasekara123@gmail.com>
2017-11-02 09:57:56 +0530

    added semester paymate and exam payment test cases

---
+++ studentManagement/src/test/java/com/csse/studentManagement/paymentTestcases/ExamPaymentTestCase.java
@@ -0,0 +1,14 @@
+package com.csse.studentManagement.paymentTestcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class ExamPaymentTestCase {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
---
+++ studentManagement/src/test/java/com/csse/studentManagement/paymentTestcases/SemesterPaymentTestCase.java
@@ -0,0 +1,14 @@
+package com.csse.studentManagement.paymentTestcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class SemesterPaymentTestCase {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
